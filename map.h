6e87d3e44690bb0a12ca3a94268b85ac194d8b07
aaaaandersen <125696997+aaaaandersen@users.noreply.github.com>
2023-04-29 22:36:51 +0800

    Add files via upload

---
+++ map.h
@@ -0,0 +1,19 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+void openmap1();
+void openmap2();
+void openmap3();
+void openmap4();
+void openmap5();
+void show_map()
+
+
+#endif
