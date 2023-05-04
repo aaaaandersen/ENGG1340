cc9c57a4b70fe4a45a74b18f7537a290478f2adb
Amia801 <128626039+Amia801@users.noreply.github.com>
2023-05-04 17:51:23 +0800

    Update save.h

--- save.h
+++ save.h
@@ -1,7 +1,13 @@
-// save.h
-#pragma once
-#include <string>
+
+#ifndef SAVE_H
+#define SAVE_H
+
 #include "player.h"
 
+// 保存玩家数据到文件
 void saveGame(const player::playerData &playerData, const std::string &filename);
-player::playerData loadGame(const std::string &filename);
+
+// 从文件加载先前保存的玩家数据
+player::playerData loadGame(const std::string &filename);  
+
+#endif
