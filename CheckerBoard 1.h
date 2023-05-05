513cf59127e1dddce52bcc934e7ef1d23331d527
aaaaandersen <125696997+aaaaandersen@users.noreply.github.com>
2023-05-06 03:48:36 +0800

    Update CheckerBoard 1.h

--- CheckerBoard 1.h
+++ CheckerBoard 1.h
@@ -15,30 +15,46 @@ using namespace std;
 
 class CheckerBoard
 {
+
 public:
+
     // Place a chess piece on the chessboard
     CheckerBoard(int size);
+    
     // Release memory
     ~CheckerBoard();
+    
     // Backtrack to place pieces
     void PlaceNode(int x, int y, int id);
+    
     // Pop
     void AddStep(ChessNode);
+    
     // Return chess piece
     ChessNode ShowNode(int x, int y);
+    
     // Backtrack step
     ChessNode ShowRecentStep();
+    
     // Regret chess piece
     void RetractNode();
+    
     // Display chess pieces on the chessboard
     void Display();
+    
     // Store path 
     void StorageRoute();
+    
     // Delete path
     void DeleteRoute();
+    
 private:
     int Size;
+    
     ChessNode** board;
+    
     stack<ChessNode> route1;
+    
     stack<ChessNode> route2;
+    
 };
